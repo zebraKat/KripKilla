f2fc8756db06f6674e715f006176fb87faf8fef9
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-25 14:11:10 -0400

    Update Krypton.lua

--- Krypton.lua
+++ Krypton.lua
@@ -201,7 +201,54 @@ do -- [[ Scoped Rig Creating. ]]
 		Attachment.Name = Name
 		Attachment.CFrame = CFrame
 		Attachment.Parent = Parent
-	end
+	endif not getgenv()["Animator"] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xhayper/Animator/main/Source/Main.lua",false))()
+	hookAnimatorFunction() -- Hook animator to Humanoid:LoadAnimation()
+end
+
+local Hats = {
+    -- ["Head"] = {
+    --     {Mesh= "rbxassetid://17517530794", Texture = "rbxassetid://17517478791", Name = "Kleos Erebus", Offset = CFrame.Angles(0,0,0)}
+    -- },
+    ["Right Arm"] = {
+        {Texture = "4645402630", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
+    },
+
+    ["Left Arm"] = {
+        {Texture = "3650139425", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
+    },
+
+    ["Right Leg"] = {
+        {Texture = "4622077774", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
+    },
+
+    ["Left Leg"] = {
+        {Texture = "3860099469", Mesh = "3030546036", Name = "InternationalFedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
+    },
+
+    ["Torso"] = {
+        {Texture = "4819722776", Mesh = "4819720316", Name = "MeshPartAccessory", Offset = CFrame.Angles(0, 0, math.rad(-15))}
+    }
+    
+}
+
+
+
+Configuration = {}
+Configuration.ReturnOnDeath = true
+Configuration.Flinging = true
+Configuration.PresetFling = Configuration.Flinging -- set to false if you gonna use the one from library
+Configuration.Animations = true
+Configuration.WaitTime = 0.22
+Configuration.RigName = "fakeRig"
+Configuration.TeleportOffsetRadius = 10
+Configuration.NoCollisions = false
+Configuration.AntiVoiding = true
+Configuration.SetSimulationRadius = true
+Configuration.DisableCharacterScripts = false
+Configuration.Hats = Hats
+local Module = game:HttpGet("https://raw.githubusercontent.com/zebraKat/KripKilla/main/Krypton.lua",false)
+loadstring(Module)()
 
 	local Torso = Instance.new("Part")
 	local RightArm = Instance.new("Part")
@@ -408,7 +455,7 @@ do -- [[ Scoped Rig Creating. ]]
 			local function OnPlatformStanding() if AnimationsToggled then Pose = "PlatformStanding" end end
 			local function OnRunning(Speed)
 					print("OnRunning")
-				if AnimationsToggled then
+				if AnimationsToggled == true then
 						print(Speed)
 					if Speed > 0.8 then
 						PlayAnimation("Walk", 0.1) Pose = "Running"

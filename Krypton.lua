27f22d7ea253f3c4c0ecd9bacbef5008efcba60e
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-25 13:46:28 -0400

    Update Krypton.lua

--- Krypton.lua
+++ Krypton.lua
@@ -368,7 +368,7 @@ do -- [[ Scoped Rig Creating. ]]
 			local function PlayAnimation(AnimName, TransitionTime)
 				local Anim = AnimationTable[AnimName].Anim
 
-				if Anim.AnimationId ~= CurrentAnimInstance.AnimationId then
+				if Anim.AnimationId ~= CurrentAnimInstance then
 					if CurrentAnimTrack ~= nil then
 						CurrentAnimTrack:Stop(TransitionTime)
 						CurrentAnimTrack:Destroy()
